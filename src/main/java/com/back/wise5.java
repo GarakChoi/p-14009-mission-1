c639caaa390d7c3f3fdf3bbbf491a701dbed25d7
tpghk <tpghks312@naver.com>
2025-05-23 12:12:44 +0900

    wise

--- src/main/java/com/back/wise5.java
+++ src/main/java/com/back/wise5.java
@@ -3,7 +3,7 @@ package com.back;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+// 5단계 목록
 public class wise5 {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
